8f836b4a446a694b5dfbdaa5c2b625f0ec859028
Gowtham S <119088234+Gowthamvishy@users.noreply.github.com>
2025-09-02 22:31:46 +0530

    Update FileController.java

--- src/main/java/org/example/backend/Controller/FileController.java
+++ src/main/java/org/example/backend/Controller/FileController.java
@@ -12,20 +12,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
-package org.example.backend.Controller;
-
-import org.example.backend.Service.FileSharerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
